f20d15eb131d8d85c382c7e9ec76976cc3f39b0e
akram6t <khanakram8435@gmail.com>
2024-09-27 22:39:46 -0400

    middleware add comment

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,10 +3,12 @@ import { NextResponse } from "next/server";
 export const middleware = () => {
     // const { pathname } = request.nextUrl;
     // const url = request.nextUrl.clone();
+    
 
     // if (pathname.startsWith('/dashboard/admin') && request.cookies.get('token')) {
     //     return NextResponse.redirect(new URL('/auth/login', request.url));
     // }
+    // else if (pathname.startsWith('/dashboard/admin') &&!request.cookies.get('token')) {
     
     
     return NextResponse.next();
